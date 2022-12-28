2152c2440873679f9b7ba41d6be2f2f2814dc847
Marcos_Davi <104923141+lastergates@users.noreply.github.com>
2022-12-28 14:22:30 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -101,7 +101,10 @@ $(document).ready(function(){
     $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
     $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
   }, 200);
-// Filtro portfólio
+
+
+
+  // Filtro portfólio
 
   $('.filter-btn').on('click', function() {
 
@@ -137,4 +140,40 @@ $(document).ready(function(){
       });
     }
   }
+
+  // Scroll para seções
+  let navBtn = $('.nav-item');
+
+  let bannerSection = $('#mainSlider');
+  let aboutSection = $('#about-area');
+  let servicesSection = $('#services-area');
+  let teamSection = $('#team-area');
+  let portfolioSection = $('#portfolio-area');
+  let contactSection = $('#contact-area');
+
+  let scrollTo ='';
+
+  $(navBtn).click(function() {
+    let btnId = $(this).attr('id');
+
+    if(btnId == 'about-menu') {
+      scrollTo = aboutSection;
+    } else if (btnId == 'services-menu'){
+      scrollTo = servicesSection;
+    } else if (btnId == 'team-menu'){
+      scrollTo = teamSection;
+    } else if (btnId == 'portfolio-menu'){
+      scrollTo = portfolioSection;
+    } else if (btnId == 'contact-menu'){
+      scrollTo = contactSection;
+    } else {
+      scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(scrollTo).offset().top - 70
+    }, 1500);
+
+  });
+
 });
